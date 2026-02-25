d24f6f52527eff0dc232dbd11e42e7ac5c4cf406
RAHUL BODDUNA <rahulbodduna21@gmail.com>
2026-02-25 09:40:00 +0530

    ES6

---
+++ app.js
@@ -0,0 +1,10 @@
+import multiply, { add,PI} from"./math.js";
+
+//document.write("Value of PI:"+PI+"<br>");
+//document.write("Addition (5 + 3):"+add(5, 3)+"<br>");
+//document.write("Multiplication (4 * 6):"+multiply(4, 6));
+
+document.body.innerHTML +=
+ "vlaue of PI: "+PI+"<br>"+
+ "Addition (5 + 3): "+add(5, 3) +"<br>"+
+ "Multiplication (4 * 6): " +multiply(4, 6);
---
+++ math.js
@@ -0,0 +1,7 @@
+export function add(a, b){
+    return a + b;
+}
+export const PI = 3.14;
+export default function mutliply(a, b){
+    return a * b;
+}
